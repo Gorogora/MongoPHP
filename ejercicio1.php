61473dde23662f47cb2a28667b4e280e5ed3eb2b
Gorogora <anagodoy03@hotmail.com>
2018-05-26 13:25:14 +0200

    Add files via upload
    Ejercicio 1 hecho

---
+++ ejercicio1.php
@@ -0,0 +1,18 @@
+<?php
+    try { 
+        $manager = new MongoDB\Driver\Manager(); 
+        echo "<p>Se ha realizado la conexión con éxito</p>";        
+    } 
+    catch (MongoDB\Driver\Exception\Exception $e) { 
+        $resultado = "<p>";
+        $resultado = $resultado. "Exception:". $e->getMessage() . "<br>"; 
+        $resultado = $resultado. "In file:". $e->getFile().  "<br>"; 
+        $resultado = $resultado. "On line:". $e->getLine(). "<br>"; 
+        $resultado = $resultado. "<p>";
+        echo $resultado;
+    } 
+?>
+
+
+
+
--- index.php
+++ index.php
@@ -20,23 +20,46 @@ and open the template in the editor.
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
         <!-- Ajax -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- JS -->
+        <script type="text/javascript" src="js/funciones.js"></script>
     </head>
     <body>
-        <?php
-            //phpinfo();
-        
-            try { 
-
-                $manager = new MongoDB\Driver\Manager("mongodb://127.0.0.1:27017"); 
-                //$manager = new MongoDB\Driver\Manager(); 
-
-            } 
-            catch (MongoDB\Driver\Exception\Exception $e) { 
-                echo "Exception:", $e->getMessage() . "<br>"; 
-                echo "In file:", $e->getFile().  "<br>"; 
-                echo "On line:", $e->getLine(). "<br>"; 
-            }  
-           
-        ?>
+        <div class="container">
+            <center><h1>PHP + MySQL</h1></center> 
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <a class="nav-link active" id="v-pills-eje1-tab" data-toggle="pill" href="#v-pills-eje1" role="tab" aria-controls="v-pills-eje1" aria-selected="true">Ejercicio 1</a>
+                    <a class="nav-link" id="v-pills-eje2-tab" data-toggle="pill" href="#v-pills-eje2" role="tab" aria-controls="v-pills-eje2" aria-selected="false">Ejercicio 2</a>
+                    <a class="nav-link" id="v-pills-eje3-tab" data-toggle="pill" href="#v-pills-eje3" role="tab" aria-controls="v-pills-eje3" aria-selected="false">Ejercicio 3</a>
+                    <a class="nav-link" id="v-pills-eje4-tab" data-toggle="pill" href="#v-pills-eje4" role="tab" aria-controls="v-pills-eje4" aria-selected="false">Ejercicio 4</a>
+                    <a class="nav-link" id="v-pills-eje5-tab" data-toggle="pill" href="#v-pills-eje5" role="tab" aria-controls="v-pills-eje5" aria-selected="false">Ejercicio 5</a>
+                    <a class="nav-link" id="v-pills-eje6-tab" data-toggle="pill" href="#v-pills-eje6" role="tab" aria-controls="v-pills-eje6" aria-selected="false">Ejercicio 6</a>
+                    <a class="nav-link" id="v-pills-eje7-tab" data-toggle="pill" href="#v-pills-eje7" role="tab" aria-controls="v-pills-eje7" aria-selected="false">Ejercicio 7</a>
+                    <a class="nav-link" id="v-pills-eje8-tab" data-toggle="pill" href="#v-pills-eje8" role="tab" aria-controls="v-pills-eje8" aria-selected="false">Ejercicio 8</a>
+                    <a class="nav-link" id="v-pills-eje9-tab" data-toggle="pill" href="#v-pills-eje9" role="tab" aria-controls="v-pills-eje9" aria-selected="false">Ejercicio 9</a>
+                    <a class="nav-link" id="v-pills-eje10-tab" data-toggle="pill" href="#v-pills-eje10" role="tab" aria-controls="v-pills-eje10" aria-selected="false">Ejercicio 10</a>
+                    <a class="nav-link" id="v-pills-eje11-tab" data-toggle="pill" href="#v-pills-eje11" role="tab" aria-controls="v-pills-eje11" aria-selected="false">Ejercicio 11</a>
+                    <a class="nav-link" id="v-pills-eje12-tab" data-toggle="pill" href="#v-pills-eje12" role="tab" aria-controls="v-pills-eje12" aria-selected="false">Ejercicio 12</a>
+                    </div>
+                </div>
+                <div class="col-md-10">
+                    <div class="tab-content" id="v-pills-tabContent">
+                        <!--        ************************** EJERCICIO 1 **************************  -->
+                        <div class="tab-pane fade show active" id="v-pills-eje1" role="tabpanel" aria-labelledby="v-pills-eje1-tab">
+                            <h2>Conexión a la base de datos</h2>
+                            <button class="btn btn-success btn-lg btn-block" type="button" onclick="ejercicio1();return false;">
+                                Conectar
+                            </button>  
+                            <div id="eje1"></div>
+                        </div>
+                        
+                        <!--        ************************** EJERCICIO 2 **************************  -->
+                        <div class="tab-pane fade" id="v-pills-eje2" role="tabpanel" aria-labelledby="v-pills-eje2-tab">
+                            
+                        </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
